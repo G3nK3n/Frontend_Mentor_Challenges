0933b26cbab948bd6b19266adc13146b6f02ec99
Ken <ken_serrano@Hotmail.com>
2021-09-29 22:48:09 -0400

    Added borders for extensions

--- bookmark-landing-page/src/Components/Extensions/ExtensionsList/ExtensionsList.js
+++ bookmark-landing-page/src/Components/Extensions/ExtensionsList/ExtensionsList.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Chrome from '../../../logo-chrome.svg';
 import Firefox from '../../../logo-firefox.svg';
 import Opera from '../../../logo-opera.svg';
+import Dots from '../../../bg-dots.svg';
+
 
 import classes from '../ExtensionsList/ExtensionsList.module.css';
+import Button from 'react-bootstrap/Button';
 
 const extensionslist = () => {
     return(
@@ -11,19 +14,25 @@ const extensionslist = () => {
             <div className={classes.OuterList}>
                 <ul className={classes.TheList}>
                     <li>
-                        <img src={Chrome} />
+                        <img className={classes.SetImages} src={Chrome} />
                         <h6>Add to chrome</h6>
                         <p>Minimum version 6.2</p>
+                        <img className={classes.Dots} src={Dots} />
+                        <Button>Add & install Extension</Button>
                     </li>
                     <li>
-                        <img src={Firefox} />
+                        <img className={classes.SetImages} src={Firefox} />
                         <h6>Add to Firefox</h6>
                         <p>Minimum version 5.5</p>
+                        <img className={classes.Dots} src={Dots} />
+                        <Button>Add & install Extension</Button>
                     </li>
                     <li>
-                        <img src={Opera} />
+                        <img className={classes.SetImages} src={Opera} />
                         <h6>Add to Opera</h6>
                         <p>Minimum version 4.6</p>
+                        <img className={classes.Dots} src={Dots} />
+                        <Button>Add & install Extension</Button>
                     </li>
                 </ul>
             </div>
