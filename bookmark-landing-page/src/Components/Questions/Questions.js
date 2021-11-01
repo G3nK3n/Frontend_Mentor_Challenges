4c96e14c8b5d6ae8fc267e3428a8d4f7c1d614e3
Ken <ken_serrano@Hotmail.com>
2021-10-31 21:46:55 -0400

    Finished Dropdown for questions

--- bookmark-landing-page/src/Components/Questions/Questions.js
+++ bookmark-landing-page/src/Components/Questions/Questions.js
@@ -4,8 +4,35 @@ import Icon from '../../icon-arrow.svg';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
-const questions = () => {
+import { useState } from 'react';
+
+const Questions = () => {
+    
+    const [getBookmark, setBookmark] = useState(false);
+    const [getRequest, setRequest] = useState(false);
+    const [getMobileApp, setMobileApp] = useState(false);
+    const [getChromium, setChromium] = useState(false);
+
+
+    const showBookmark = () => {
+        setBookmark(old => !old);
+    }
+    
+    const showRequest = () => {
+        setRequest(old => !old);
+    }
+    
+    const showMobileApp = () => {
+        setMobileApp(old => !old);
+    }
+    
+    const showChromium = () => {
+        setChromium(old => !old);
+    }
+
+
     return(
         <div className={classes.Questions}>
             <Row> 
@@ -20,37 +47,73 @@ const questions = () => {
                         <ul>
                             <li>
                                 <div className={classes.InnerList}>
-                                    <a href="javascript:void(0);">
+                                    <a onClick={showBookmark} href="javascript:void(0);">
                                         <h6>What is Bookmark?</h6>
                                         <img src={Icon} />
                                     </a>
+                                    {getBookmark===true &&
+                                    
+                                        <div className={classes.getBookmark}>
+                                            <p>
+                                                This section will be for bookmark information!
+                                            </p>
+                                        </div>
+                                    }
                                 </div>
+
                             </li>
                             <li>
                                 <div className={classes.InnerList}>
-                                    <a href="javascript:void(0);">
+                                    <a onClick={showRequest} href="javascript:void(0);">
                                         <h6>How can I request a new browser?</h6>
                                         <img src={Icon} />
                                     </a>
+                                    {getRequest===true &&
+                                    
+                                    <div className={classes.getBookmark}>
+                                        <p>
+                                            This section will be for request information!
+                                        </p>
+                                    </div>
+                                }
                                 </div>
                             </li>
                             <li>
                                 <div className={classes.InnerList}>
-                                    <a href="javascript:void(0);">
+                                    <a onClick={showMobileApp} href="javascript:void(0);">
                                         <h6>Is there a mobile app?</h6>
                                         <img src={Icon} />
                                     </a>
+                                    {getMobileApp===true &&
+                                    
+                                    <div className={classes.getBookmark}>
+                                        <p>
+                                            This section will be for mobile App information!
+                                        </p>
+                                    </div>
+                                }
                                 </div>
                             </li>
                             <li>
                                 <div className={classes.InnerList}>
-                                    <a href="javascript:void(0);">
+                                    <a onClick={showChromium} href="javascript:void(0);">
                                         <h6>What about other Chromium browsers?</h6>
                                         <img src={Icon} />
                                     </a>
+                                    {getChromium===true &&
+                                    
+                                    <div className={classes.getBookmark}>
+                                        <p>
+                                            This section will be for chromium information!
+                                        </p>
+                                    </div>
+                                }
                                 </div>
                             </li>
                         </ul>
+                    </div>
+                    <div className={classes.MoreInfoButton}>
+                        <Button>More Info</Button>
                     </div>
                 </Col>
             </Row>
@@ -59,4 +122,4 @@ const questions = () => {
     )
 }
 
-export default questions;
+export default Questions;
