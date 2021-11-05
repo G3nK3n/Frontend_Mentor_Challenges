89db602ce120b7129987221a552e52e033241534
Ken <ken_serrano@Hotmail.com>
2021-11-04 23:16:24 -0400

    Fixes for Contact component

--- bookmark-landing-page/src/Components/Contact/Contact.js
+++ bookmark-landing-page/src/Components/Contact/Contact.js
@@ -1,9 +1,18 @@
 import React from "react";
+import classes from '../Contact/Contact.module.css';
+import Button from 'react-bootstrap/Button';
 
 const contact = () => {
     return(
-        <div>
-            <h1>This is contact page!</h1>
+        <div className={classes.OuterContact}>
+            <div className={classes.ContactText}>
+                <p>35,000+ already joined</p>
+                <h1>Stay up-to-date with that we're doing</h1>
+            </div>
+            <div className={classes.ContactForm}>
+                <input type="text" />
+                <Button>Contact Us</Button>
+            </div>
         </div>
     );
 }
