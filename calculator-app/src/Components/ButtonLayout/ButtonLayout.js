177610c57eb9ee1981d30f25e49855117bb63031
Ken <ken_serrano@Hotmail.com>
2021-11-28 18:59:01 -0500

    Fixed Delete button and fixes on style

--- calculator-app/src/Components/ButtonLayout/ButtonLayout.js
+++ calculator-app/src/Components/ButtonLayout/ButtonLayout.js
@@ -13,30 +13,57 @@ const ButtonLayout = () => {
         setOutput(previous+e.target.value);
     }
 
+    const deleteNumber = () => {
+        let previous = output.slice(0, output.length-1);
+        setOutput(previous);
+    }
+
+    const resetCalculator = () => {
+        setOutput('');
+    }
+
+    //NOTE: You cannot use a callback event(Such as onClick) on a Custom component. Worse comes to worse, 
+    //you can just surround that custom component with a div and add the callback event there(Lazy way?)
     return(
         <div className={classes.OuterButtonLayout}>
             <div className={classes.ButtonLayout}>
                 <Buttons testClick={outputScreen}>7</Buttons>
                 <Buttons testClick={outputScreen}>8</Buttons>
                 <Buttons testClick={outputScreen}>9</Buttons>
-                <Buttons>DEL</Buttons>
+                <div onClick={deleteNumber}>
+                    <Buttons>DEL</Buttons>
+                </div>
                 <Buttons testClick={outputScreen}>4</Buttons>
                 <Buttons testClick={outputScreen}>5</Buttons>
                 <Buttons testClick={outputScreen}>6</Buttons>
-                <Buttons>+</Buttons>
+                <div>
+                    <Buttons>+</Buttons>
+                </div>
                 <Buttons testClick={outputScreen}>1</Buttons>
                 <Buttons testClick={outputScreen}>2</Buttons>
                 <Buttons testClick={outputScreen}>3</Buttons>
-                <Buttons>-</Buttons>
-                <Buttons>.</Buttons>
+                <div>
+                    <Buttons>-</Buttons>
+                </div>
+                <div>
+                    <Buttons>.</Buttons>
+                </div>
                 <Buttons testClick={outputScreen}>0</Buttons>
-                <Buttons>/</Buttons>
-                <Buttons>x</Buttons>
+                <div>
+                    <Buttons>/</Buttons>
+                </div>
+                <div>
+                    <Buttons>x</Buttons>
+                </div>
                 <p>{output}</p>
             </div>
             <div className={classes.ButtonLayout2}>
-                <Buttons2>RESET</Buttons2>
-                <Buttons2>=</Buttons2>
+                <div onClick={resetCalculator}>
+                    <Buttons2>RESET</Buttons2>
+                </div>
+                <div>
+                    <Buttons2>=</Buttons2>
+                </div>
             </div>
         </div>
     )
