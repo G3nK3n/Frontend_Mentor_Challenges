c2d48fed1b2b1dd5a44ccf2bb0c3c9f60df82873
Ken <ken_serrano@Hotmail.com>
2021-12-02 22:44:12 -0500

    Started calculations

--- calculator-app/src/Components/ButtonLayout/ButtonLayout.js
+++ calculator-app/src/Components/ButtonLayout/ButtonLayout.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import Buttons from '../Buttons/Buttons';
 import Buttons2 from '../Buttons/Buttons2';
 
@@ -7,7 +7,7 @@ import classes from '../ButtonLayout/ButtonLayout.module.css';
 const ButtonLayout = () => {
     
     const [output, setOutput] = useState('');
-
+    const [storedNumbers, setStoredNumbers] = useState(0);
     const outputScreen = (e) => {
         let previous = output;
         setOutput(previous+e.target.value);
@@ -20,8 +20,46 @@ const ButtonLayout = () => {
 
     const resetCalculator = () => {
         setOutput('');
+        setStoredNumbers('');
     }
 
+    useEffect(() => {
+        setStoredNumbers(0);
+    }, [])
+
+    const calculating = (e) => {
+        let tempNumber = storedNumbers;
+        let symbol = String(e.target.value);
+        let previous = tempNumber;
+
+        //Arithmetic seems off, fix this. Ex: When pressing '-' when you wanted to do '+' will result to subtraction instead.
+        if(tempNumber==0) {
+            setStoredNumbers(output);
+            setOutput('');
+        }
+        else if(symbol === "+") {
+            setStoredNumbers(String(Number(previous) + Number(output)));
+            setOutput('');
+        }
+        else if(symbol === "-") {
+            setStoredNumbers(String(Number(previous) - Number(output)));
+            setOutput('');
+        }
+        else if(symbol === "x") {
+            setStoredNumbers(String(Number(previous) * Number(output)));
+            setOutput('');
+        }
+        else if(symbol === "/") {
+            setStoredNumbers(String(Number(previous) - Number(output)));
+            setOutput('');
+        }
+
+        
+    }
+
+    // useEffect(() => {
+    //     setStoredNumbers('');
+    // }, [])
     //NOTE: You cannot use a callback event(Such as onClick) on a Custom component. Worse comes to worse, 
     //you can just surround that custom component with a div and add the callback event there(Lazy way?)
     return(
@@ -36,26 +74,27 @@ const ButtonLayout = () => {
                 <Buttons testClick={outputScreen}>4</Buttons>
                 <Buttons testClick={outputScreen}>5</Buttons>
                 <Buttons testClick={outputScreen}>6</Buttons>
-                <div>
+                <div onClick={calculating}>
                     <Buttons>+</Buttons>
                 </div>
                 <Buttons testClick={outputScreen}>1</Buttons>
                 <Buttons testClick={outputScreen}>2</Buttons>
                 <Buttons testClick={outputScreen}>3</Buttons>
-                <div>
+                <div onClick={calculating}>
                     <Buttons>-</Buttons>
                 </div>
                 <div>
                     <Buttons>.</Buttons>
                 </div>
                 <Buttons testClick={outputScreen}>0</Buttons>
-                <div>
+                <div onClick={calculating}>
                     <Buttons>/</Buttons>
                 </div>
-                <div>
+                <div onClick={calculating}>
                     <Buttons>x</Buttons>
                 </div>
                 <p>{output}</p>
+                {storedNumbers}
             </div>
             <div className={classes.ButtonLayout2}>
                 <div onClick={resetCalculator}>
