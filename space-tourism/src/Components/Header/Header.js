a6cd31fbbbbb504974d36a4e95ad7548699c788e
G3nK3n <ken_serrano@Hotmail.com>
2022-03-28 22:49:05 -0400

    Hide header on small screens

--- space-tourism/src/Components/Header/Header.js
+++ space-tourism/src/Components/Header/Header.js
@@ -2,25 +2,32 @@ import React from "react";
 import Logo from '../../logo.svg';
 import classes from './Header.module.css';
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Header = () => {
     return (
         <div>
-            <div className={classes.Logo}>
-                <img src={Logo} />
-            </div>
-            
-            {/* <div className={classes.whiteLine}></div> */}
+            <Row>
+                <Col className="d-none d-md-block">
+                    {/* d-none d-lg-block */}
+                    <div className={classes.Logo}>
+                        <img src={Logo} />
+                    </div>
+                
 
-            <div className={classes.tempDiv}></div>
-            <div className={classes.Links}>
-                <ul>
-                    <li><a href="#"><span><div><b>00</b>Home</div></span></a></li>
-                    <li><a href="#"><span><div><b>01</b>Destination</div></span></a></li>
-                    <li><a href="#"><span><div><b>02</b>Crew</div></span></a></li>
-                    <li><a href="#"><span><div><b>03</b>Technology</div></span></a></li>
-                </ul>
-            </div>
-
+                    <div className={classes.tempDiv}></div>
+                
+                    <div className={classes.Links}>
+                        <ul>
+                            <li><a href="#"><span><div><b>00</b>Home</div></span></a></li>
+                            <li><a href="#"><span><div><b>01</b>Destination</div></span></a></li>
+                            <li><a href="#"><span><div><b>02</b>Crew</div></span></a></li>
+                            <li><a href="#"><span><div><b>03</b>Technology</div></span></a></li>
+                        </ul>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 }
