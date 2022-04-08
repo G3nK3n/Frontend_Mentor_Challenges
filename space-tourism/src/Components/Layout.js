ace9394af3997890bf09f1a5d79cd08852ee556a
G3nK3n <ken_serrano@Hotmail.com>
2022-04-07 21:56:10 -0400

    Added modal

--- space-tourism/src/Components/Layout.js
+++ space-tourism/src/Components/Layout.js
@@ -2,12 +2,23 @@ import React from "react";
 import Home from './Home/Home';
 import Header from './Header/Header';
 import classes from './Layout.module.css';
+import Modal from './Modal/Modal';
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Layout = () => {
     return(
         <div className={classes.BackgroundImage}>
             <Header />
             <Home />
+            <Row>
+                <Col className="d-block d-sm-none">
+                    {/* Only appear in SM and X-SM */}
+                    <Modal />    
+                </Col>   
+            </Row>
+            
         </div>
     )
 }
---
+++ space-tourism/src/Components/Modal/Modal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from './Modal.module.css';
+
+const Modal = () => {
+    return(
+        <div className={classes.Modal}>
+            <h1>Some Text</h1>
+        </div>
+    )
+}
+
+export default Modal;
