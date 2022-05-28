f9cf20175f3bad8a950b974d3b86220c39986832
G3nK3n <ken_serrano@Hotmail.com>
2022-05-27 23:02:38 -0400

    Fixed Destination component

--- space-tourism/src/Components/Destinations/Destination.js
+++ space-tourism/src/Components/Destinations/Destination.js
@@ -1,7 +1,8 @@
 import React from "react";
 import classes from "./Destination.module.css";
+import Moon from './Moon/Moon';
 
-import { Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 const Destination = () => {
     return(
@@ -14,6 +15,11 @@ const Destination = () => {
                     <li><Link to={"/destination/europa"}><div>Europa</div></Link></li>
                     <li><Link to={"/destination/titan"}><div>Titan</div></Link></li>
                 </ul>
+                <Switch>
+                    <Route exact path='/destination/moon'>
+                        <Moon />
+                    </Route>
+                </Switch>
             </div>
         </div>
     )
---
+++ space-tourism/src/Components/Destinations/Moon/Moon.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Moon = () => {
+    return(
+        <div>
+            <h1>Moon</h1>
+            <p>
+                See our planet as you've never seen it before.
+                A perfect relaxing trip away to help regain perspective and come back refreshed.
+                While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+            </p>
+        </div>
+    )
+}
+
+export default Moon;
