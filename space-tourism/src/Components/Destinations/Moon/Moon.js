9d070f50c3b85528069bab874f8ed21eb6bdfb6a
G3nK3n <ken_serrano@Hotmail.com>
2022-05-29 23:18:00 -0400

    Fixed styles for Moon

--- space-tourism/src/Components/Destinations/Moon/Moon.js
+++ space-tourism/src/Components/Destinations/Moon/Moon.js
@@ -1,14 +1,27 @@
 import React from "react";
+import classes from "./Moon.module.css";
 
 const Moon = () => {
     return(
-        <div>
-            <h1>Moon</h1>
-            <p>
-                See our planet as you've never seen it before.
-                A perfect relaxing trip away to help regain perspective and come back refreshed.
-                While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
-            </p>
+        <div className={classes.Moon}>
+            <div className={classes.MainText}>
+                <h1>Moon</h1>
+                <p className={classes.Paragraphs}>
+                    See our planet as you've never seen it before.
+                    A perfect relaxing trip away to help regain perspective and come back refreshed.
+                    While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.
+                </p>
+            </div>
+            
+            <div className={classes.Description}>
+                <h5>Avg. Distance</h5>
+                <p>384,400 KM</p>
+            </div>
+            <div className={classes.Description}>
+                <h5>Est. Travel Time</h5>
+                <p>3 Days</p>
+            </div>
+            
         </div>
     )
 }
