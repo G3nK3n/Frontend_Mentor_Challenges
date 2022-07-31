cc56c8831131b4ddc789f20ebfc1aebc1fd2876e
G3nK3n <ken_serrano@Hotmail.com>
2022-07-30 23:17:52 -0400

    Fixed Mars and Europa Components

--- space-tourism/src/Components/Destinations/Europa/Europa.js
+++ space-tourism/src/Components/Destinations/Europa/Europa.js
@@ -2,30 +2,61 @@ import React from "react";
 import classes from "./Europa.module.css";
 import Img1 from "../../../image-europa.png";
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Europa = () => {
     return(
         <div className={classes.Europa}>
-            <div className={classes.MainText}>
-                <h1>Europa</h1>
-                <p className={classes.Paragraphs}>
-                    The smalles of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream.
-                    With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation
-                    in your snug wintery cabin.
-                </p>
-            </div>
-            
-            <div className={classes.Description}>
-                <h5>Avg. Distance</h5>
-                <p>628 MIL KM</p>
-            </div>
-            <div className={classes.Description}>
-                <h5>Est. Travel Time</h5>
-                <p>3 YEARS</p>
-            </div>
-            <div className={classes.Image}>
-                <img src={Img1} />
-            </div>
-            
+            <Row>
+                <Col className="d-none d-lg-block">
+                    <div className={classes.MainText}>
+                        <h1>Europa</h1>
+                        <p className={classes.Paragraphs}>
+                            The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream.
+                            With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.
+                        </p>
+                    </div>
+                    
+                    <div className={classes.Description}>
+                        <h5>Avg. Distance</h5>
+                        <p>628 MIL. KM</p>
+                    </div>
+                    <div className={classes.Description}>
+                        <h5>Est. Travel Time</h5>
+                        <p>3 YEARS</p>
+                    </div>
+                    <div className={classes.Image}>
+                        <img src={Img1} />
+                    </div>    
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-sm-block d-md-block d-lg-none">
+                    
+                    <div className={classes.Image}>
+                        <img src={Img1} />
+                    </div>  
+                    <div className={classes.MainText}>
+                        <h1>Europa</h1>
+                        <p className={classes.Paragraphs}>
+                            The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream.
+                            With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.
+                        </p>
+                    </div>
+
+                    <div className={classes.outerDescription}>
+                        <div className={classes.Description}>
+                            <h5>Avg. Distance</h5>
+                            <p>628 MIL. KM</p>
+                        </div>
+                        <div className={classes.Description}>
+                            <h5>Est. Travel Time</h5>
+                            <p>3 YEARS</p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     )
 }
--- space-tourism/src/Components/Destinations/Mars/Mars.js
+++ space-tourism/src/Components/Destinations/Mars/Mars.js
@@ -2,29 +2,61 @@ import React from "react";
 import classes from "./Mars.module.css";
 import Img1 from "../../../image-mars.png";
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Mars = () => {
     return(
         <div className={classes.Mars}>
-            <div className={classes.MainText}>
-                <h1>Mars</h1>
-                <p className={classes.Paragraphs}>
-                    Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the
-                    tallest planetary mountain in our solar system. It's two and a half times the size of Everest!
-                </p>
-            </div>
-            
-            <div className={classes.Description}>
-                <h5>Avg. Distance</h5>
-                <p>225 MIL KM</p>
-            </div>
-            <div className={classes.Description}>
-                <h5>Est. Travel Time</h5>
-                <p>9 MONTHS</p>
-            </div>
-            <div className={classes.Image}>
-                <img src={Img1} />
-            </div>
-            
+            <Row>
+                <Col className="d-none d-lg-block">
+                    <div className={classes.MainText}>
+                        <h1>Mars</h1>
+                        <p className={classes.Paragraphs}>
+                            Dont't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system.
+                            It's two and a half times the sizes of Everest.
+                        </p>
+                    </div>
+                    
+                    <div className={classes.Description}>
+                        <h5>Avg. Distance</h5>
+                        <p>225 MIL. KM</p>
+                    </div>
+                    <div className={classes.Description}>
+                        <h5>Est. Travel Time</h5>
+                        <p>9 MONTHS</p>
+                    </div>
+                    <div className={classes.Image}>
+                        <img src={Img1} />
+                    </div>    
+                </Col>
+            </Row>
+            <Row>
+                <Col className="d-sm-block d-md-block d-lg-none">
+                    
+                    <div className={classes.Image}>
+                        <img src={Img1} />
+                    </div>  
+                    <div className={classes.MainText}>
+                        <h1>Mars</h1>
+                        <p className={classes.Paragraphs}>
+                            Dont't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system.
+                            It's two and a half times the sizes of Everest.
+                        </p>
+                    </div>
+
+                    <div className={classes.outerDescription}>
+                        <div className={classes.Description}>
+                            <h5>Avg. Distance</h5>
+                            <p>225 MIL. KM</p>
+                        </div>
+                        <div className={classes.Description}>
+                            <h5>Est. Travel Time</h5>
+                            <p>9 MONTHS</p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
         </div>
     )
 }
