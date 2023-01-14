7d2c556544ec559b38824af66ab57294eb46a712
G3nK3n <ken_serrano@Hotmail.com>
2023-01-14 00:18:43 -0500

    Started pay-api

---
+++ pay-api/src/App.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from './Components/Layout';
+
+function App() {
+  return (
+    <div>
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
---
+++ pay-api/src/Components/Header/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import classes from '../Header/Header.module.css';
+
+import LogoImage from '../../logo.svg';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+    return(
+        <div className={classes.MainHeaderDiv}>
+            <Container>
+                <div className={classes.LeftSideLinks}>
+                    <img src={LogoImage} />
+                </div>
+                <ul>
+                        <li>Pricing</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                <div className={classes.RightSideLinks}>
+                    <Button variant="primary">Schedule a Demo</Button>
+                </div>
+            </Container>
+            
+        </div>
+    )
+}
+
+export default Header;
