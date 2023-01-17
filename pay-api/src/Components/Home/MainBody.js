50f7194adacdbdcb60d511d1ab36fc14bd6e4695
G3nK3n <ken_serrano@Hotmail.com>
2023-01-16 22:00:51 -0500

    Fixes MainBody

--- pay-api/src/Components/Home/MainBody.js
+++ pay-api/src/Components/Home/MainBody.js
@@ -11,17 +11,19 @@ const MainBody = () => {
         <div className={classes.MainBodyDiv}>
             <Header />
             <Container>
-                <div>
-                    <h1>Start building with our APIs for absolutely free</h1>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="formBasicRequest">
-                            <Form.Control type="email" placeholder="Enter email address" />
-                        </Form.Group>
-                        <div className={classes.ScheduleDemo}>
-                            <Button variant="primary">Schedule a Demo</Button>
-                        </div>
-                    </Form>
-                    <p>Have any questions? Contact Us</p>
+                <div className={classes.MainBodyContents}>
+                    <div className={classes.LeftSideContents}>
+                        <h1>Start building with our APIs for absolutely free</h1>
+                        <Form>
+                            <Form.Group className="mb-3" controlId="formBasicRequest">
+                                <Form.Control type="email" placeholder="Enter email address" />
+                            </Form.Group>
+                            <div className={classes.ScheduleDemo}>
+                                <Button variant="primary">Schedule a Demo</Button>
+                            </div>
+                        </Form>
+                        <p>Have any questions? Contact Us</p>
+                    </div>
                 </div>
             </Container>
         </div>
