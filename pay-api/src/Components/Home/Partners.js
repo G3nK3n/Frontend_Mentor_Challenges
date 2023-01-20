0007009fe1f4488ad2e28e270ccebe860c132e6b
G3nK3n <ken_serrano@Hotmail.com>
2023-01-19 23:44:08 -0500

    Started Partners section

--- pay-api/src/Components/Home/Partners.js
+++ pay-api/src/Components/Home/Partners.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+
+import classes from '../Home/Partners.module.css';
+
 const Partners = () => {
     return(
-        <div>
-            <h1>Partners</h1>
+        <div className={classes.PartnersDiv}>
+            <Container>
+                <div className={classes.LeftSideContent}>
+                    <h1>Who we work with</h1>
+                    <p>
+                        Today, millions of people around the world have successfully connected their accounts to apps they love using our API. 
+                        We provided developers with the tools they need to create easy and accessible experiences for their users.
+                    </p>
+                    <Button>About Us</Button>
+                </div>
+            </Container>
         </div>
     )
 }
