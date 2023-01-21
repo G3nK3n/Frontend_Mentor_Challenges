8719e4957d278f47d26798f34cff3097cf1b66ea
G3nK3n <ken_serrano@Hotmail.com>
2023-01-20 23:51:59 -0500

    Fixes on Partners

--- pay-api/src/Components/Home/Partners.js
+++ pay-api/src/Components/Home/Partners.js
@@ -2,8 +2,17 @@ import React from "react";
 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
 
 import classes from '../Home/Partners.module.css';
+
+import Google from '../../google.svg';
+import HP from '../../hewlett-packard.svg';
+import Microsoft from '../../microsoft.svg';
+import Nvidia from '../../nvidia.svg';
+import Oracle from '../../oracle.svg';
+import Tesla from '../../tesla.svg';
+
 
 const Partners = () => {
     return(
@@ -16,6 +25,22 @@ const Partners = () => {
                         We provided developers with the tools they need to create easy and accessible experiences for their users.
                     </p>
                     <Button>About Us</Button>
+                </div>
+                <div className={classes.RightSideContent}>
+                    <Table size="sm" className={classes.RightTable} borderless>
+                        <tbody>
+                            <tr>
+                                <td><img src={Tesla} /></td>
+                                <td><img src={Microsoft} /></td>
+                                <td><img src={HP} /></td>
+                            </tr>
+                            <tr>
+                                <td><img src={Oracle} /></td>
+                                <td><img src={Google} /></td>
+                                <td><img src={Nvidia} /></td>
+                            </tr>
+                        </tbody>
+                    </Table>
                 </div>
             </Container>
         </div>
